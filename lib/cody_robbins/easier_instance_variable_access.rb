755d3aec431c8624ad0ca4c66f81760a324f91bf
Cody Robbins <cody@sakuzaku.com>
2011-05-23 17:33:24 -0400

    Added some documentation.

--- lib/cody_robbins/easier_instance_variable_access.rb
+++ lib/cody_robbins/easier_instance_variable_access.rb
@@ -1,10 +1,20 @@
+# encoding: UTF-8
+
 module CodyRobbins
   module EasierInstanceVariableAccess
-    def get_instance_variable(name)
+    # Returns the value of the specified instance variable, just like `Object#instance_variable_get`.
+    #
+    # @return The value of the specified instance variable.
+    # @param name [Symbol, String] The name of the instance variable to retrieve—without the leading ‘@’.
+    def instance_variable(name)
       instance_variable_get(instance_variable_name(name))
     end
-    alias :instance_variable :get_instance_variable
+    alias :get_instance_variable :instance_variable
 
+    # Sets the value of the specified instance variable, just like `Object#instance_variable_set`.
+    #
+    # @param name [Symbol, String] The name of the instance variable to set—without the leading ‘@’.
+    # @param value The value to set the specified instance variable to.
     def set_instance_variable(name, value)
       instance_variable_set(instance_variable_name(name), value)
     end
